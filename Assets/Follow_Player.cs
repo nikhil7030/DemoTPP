3495bc35d7b891a6f08b79bd71b1dbc233897fb4
nikhil7030 <37480239+nikhil7030@users.noreply.github.com>
2019-07-28 22:35:15 +0530

    FirstCommit

---
+++ Assets/Follow_Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow_Player : MonoBehaviour
+{
+    GameObject[] _player;
+    Vector3 player_Position;
+    public Vector3 OffSet;
+    
+
+    void Start()
+    {
+        _player = GameObject.FindGameObjectsWithTag("Player");
+        player_Position = new Vector3();
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        
+        player_Position = _player[0].transform.position + OffSet;
+        transform.position = player_Position;
+        
+    }
+}
---
+++ Assets/player_Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_Movement : MonoBehaviour
+{
+    public Rigidbody Player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("w"))
+        {
+            Player.AddForce(0,0,20);
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            Player.AddForce(5,0,20);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            Player.AddForce(-5,0,20);
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            Player.AddForce(0,0,-20);
+        }
+    }
+}
